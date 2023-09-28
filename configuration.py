6357230b2e61aebe35e846f61b78f5305c569c4d
Fernanda Rampad <claudiafer_1997@hotmail.com>
2023-09-27 20:43:28 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://4b042f0c-0dd1-443c-8257-4d9c687a7e6c.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
